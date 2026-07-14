93f0c44bc18f7100432faa7f08ba44a34e8ece32
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2026-07-14 16:13:02 +0200

    Serve localized pricing as JavaScript

--- pricing.test.js
+++ pricing.test.js
@@ -1,7 +1,7 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
 
-import { premiumPriceForLocales } from './pricing.mjs'
+import { premiumPriceForLocales } from './pricing.js'
 
 test('localizes the configured Stripe prices', () => {
   assert.equal(premiumPriceForLocales(['nb-NO']), '89 NOK')
